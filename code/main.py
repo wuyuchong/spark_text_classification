4460e3c8157a3692586bcdb90534b582187fcc4b
wuyuchong <vector_wyc@icloud.com>
2022-01-07 21:16:01 +0800

    finish model tuning

--- code/main.py
+++ code/main.py
@@ -8,6 +8,9 @@ from pyspark.ml import Pipeline
 from pyspark.ml.feature import Tokenizer,StopWordsRemover,CountVectorizer,IDF
 from pyspark.ml.feature import StringIndexer
 from pyspark.ml.classification import LogisticRegression
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+from pyspark.ml.tuning import CrossValidator,ParamGridBuilder
+from pyspark.sql.types import StringType
 
 
 # ----------------------------> 数据读取
@@ -23,15 +26,15 @@ pandasDF.isnull().sum() # 缺失值检查
 df = spark.createDataFrame(pandasDF)
 df.printSchema()
 df.show()
-df.count()
+df.groupBy('sentiment').count().show()
 # ----------------------------------------------------------------------
 
 
-# ----------------------------> 文本特征化和标签转换
-tokenizer = Tokenizer(inputCol='review', outputCol='tokens')
-stopwords_remover = StopWordsRemover(inputCol='tokens', outputCol='filtered_tokens')
-vectorizer = CountVectorizer(inputCol='filtered_tokens', outputCol='rawFeatures')
-idf = IDF(inputCol='rawFeatures', outputCol='vectorizedFeatures')
+# ----------------------------> 文本清洁
+# ----------------------------------------------------------------------
+
+
+# ----------------------------> 标签数字转换
 labelEncoder = StringIndexer(inputCol='sentiment', outputCol='label').fit(df)
 labelEncoder.transform(df).show(5)
 df = labelEncoder.transform(df)
@@ -43,15 +46,63 @@ df = labelEncoder.transform(df)
 # ----------------------------------------------------------------------
 
 
-# ----------------------------> 模型
-lr = LogisticRegression(featuresCol='vectorizedFeatures',labelCol='label')
-pipeline = Pipeline(stages=[tokenizer,stopwords_remover,vectorizer,idf,lr])
-lr_model = pipeline.fit(trainDF)
-predictions = lr_model.transform(testDF)
-predictions.show()
-predictions.select(['label', 'prediction']).show()
+# ----------------------------> 文本特征化
+tokenizer = Tokenizer(inputCol='review', outputCol='tokens')
+add_stopwords = ["<br />","amp"]
+stopwords_remover = StopWordsRemover(inputCol='tokens', outputCol='filtered_tokens').setStopWords(add_stopwords)
+vectorizer = CountVectorizer(inputCol='filtered_tokens', outputCol='rawFeatures')
+# or use hashingTF = HashingTF(inputCol="tokens", outputCol="rawFeatures")
+idf = IDF(inputCol='rawFeatures', outputCol='vectorizedFeatures')
+pipeline = Pipeline(stages=[tokenizer,stopwords_remover,vectorizer,idf])
+preprocessModel = pipeline.fit(trainDF)
+trainDF = preprocessModel.transform(trainDF)
+testDF = preprocessModel.transform(testDF)
 # ----------------------------------------------------------------------
 
 
-# ----------------------------> reference
-#  https://www.section.io/engineering-education/multiclass-text-classification-with-pyspark/
+# ----------------------------> 训练
+lr = LogisticRegression(featuresCol='vectorizedFeatures',labelCol='label')
+lr_model = lr.fit(trainDF)
+predictions = lr_model.transform(testDF)
+predictions.select(['label', 'prediction']).show()
+evaluator = MulticlassClassificationEvaluator(labelCol='label',predictionCol='prediction',metricName='accuracy')
+accuracy = evaluator.evaluate(predictions)
+accuracy
+# precision recall .... here 
+# ----------------------------------------------------------------------
+
+
+# ----------------------------> 预测
+inputText = spark.createDataFrame([("I like this movie",StringType()),
+                                   ("Back of Beyond",StringType())],
+                                  ["review"])
+inputText.show(truncate=False)
+inputText = preprocessModel.transform(inputText)
+inputPrediction = lr_model.transform(inputText)
+inputPrediction.show()
+inputPrediction.select(['review', 'prediction']).show()
+# ----------------------------------------------------------------------
+
+
+# ----------------------------> 模型调参
+def logisticCV(trainDF, testDF):
+    lr = LogisticRegression(featuresCol='vectorizedFeatures',labelCol='label')
+    pipeline = Pipeline(stages=[lr])
+    paramGrid = ParamGridBuilder() \
+        .addGrid(lr.regParam, [0.01, 0.5, 2.0]) \
+        .addGrid(lr.elasticNetParam, [0.0, 0.5, 1.0]) \
+        .addGrid(lr.maxIter, [1, 5, 10]) \
+        .build() 
+    evaluator = MulticlassClassificationEvaluator(labelCol='label',predictionCol='prediction',metricName='accuracy')
+    crossValidator = CrossValidator(estimator=pipeline, 
+                                    evaluator=evaluator,
+                                    estimatorParamMaps=paramGrid,
+                                    numFolds=5)
+    cv = crossValidator.fit(trainDF)
+    best_model = cv.bestModel.stages[0]
+    prediction = best_model.transform(test_df)
+    accuracy = evaluator.evaluate(predictions)
+    print('Accuracy in Cross Validation of logistic regression: %g' % accuracy)
+
+logisticCV(trainDF, testDF)
+
